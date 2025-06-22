2881001390685644ef65c72c6dfa1898abc78287
bhaveshkapade199 <123622500+bhaveshkapade199@users.noreply.github.com>
2025-06-22 22:44:46 +0530

    Some New changes add

--- src/app/navigation-bar/home/home.ts
+++ src/app/navigation-bar/home/home.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faInstagram, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+   
+  faInstagram=faInstagram;
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
 }
