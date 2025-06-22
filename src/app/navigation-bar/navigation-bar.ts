821ee66d4d1f9ebb7ce12764dcfe156c07203ae7
bhaveshkapade199 <123622500+bhaveshkapade199@users.noreply.github.com>
2025-06-22 16:41:45 +0530

    new changes add

--- src/app/navigation-bar/navigation-bar.ts
+++ src/app/navigation-bar/navigation-bar.ts
@@ -1,15 +1,22 @@
-import { CommonModule } from '@angular/common';
+
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navigation-bar',
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,RouterLink, FontAwesomeModule ],
   templateUrl: './navigation-bar.html',
   styleUrl: './navigation-bar.css'
 })
 export class NavigationBar {
      
+    faBars = faBars;
+    faSignOutALt = faSignOutAlt;
+    
+    
     router = inject(Router)  
     
 
